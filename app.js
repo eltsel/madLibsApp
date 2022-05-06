82ac743f9655d371db5bb7761f83c691d69a4c50
Eleni Tselioy <eltselioy@gmail.com>
2022-05-06 12:42:12 +0300

    add paragraph

---
+++ app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+let userInputs = ["woman","marriage","family","childbirth","twelve","Zeus","married women","weddings"];
+let originalStory = `Hera is the goddess of ${userInputs[0]}, ${userInputs[1]}, ${userInputs[2]} and ${userInputs[3]} in ancient Greek religion and mythology, one of the ${userInputs[4]} Olympians and the sister and wife of ${userInputs[5]}. 
+
+She is the daughter of the Titans Cronus and Rhea. Hera rules over Mount Olympus as queen of the gods. A matronly figure, Hera served as both the patroness and protectress of ${userInputs[6]} women, presiding over ${userInputs[7]} and blessing marital unions.`
+
+console.log(originalStory);
+
+
+
+/*`Hera is the goddess of women, marriage, family and childbirth in ancient Greek religion and mythology, one of the twelve Olympians and the sister and wife of Zeus. She is the daughter of the Titans Cronus and Rhea. Hera rules over Mount Olympus as queen of the gods. A matronly figure, Hera served as both the patroness and protectress of married women, presiding over weddings and blessing marital unions. */
