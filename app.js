c946c5171fb9ba1451ffd13c85fc32c239997df0
Eleni Tselioy <eltselioy@gmail.com>
2022-05-07 22:26:24 +0300

    final css touches

--- app.js
+++ app.js
@@ -18,9 +18,7 @@ for(let i = numberOfQuestions; i >= 0; i--){
 }
 
 // Edit Paragraph
-let originalStory = `<div class="flex">Hera is the goddess of ${userInputs[0]}, ${userInputs[1]}, ${userInputs[2]} and ${userInputs[3]} in ancient Greek religion and mythology, one of the ${userInputs[4]} Olympians and the sister and wife of ${userInputs[5]}. 
-
-She is the daughter of the Titans Cronus and Rhea. Hera rules over Mount Olympus as queen of the gods. A matronly figure, Hera served as both the patroness and protectress of ${userInputs[6]} women, presiding over ${userInputs[7]} and blessing marital unions.</div>`
+let originalStory = `Hera is the goddess of ${userInputs[0]}, ${userInputs[1]}, ${userInputs[2]} and ${userInputs[3]} in ancient Greek religion and mythology, one of the ${userInputs[4]} Olympians and the sister and wife of ${userInputs[5]}. <br><br>She is the daughter of the Titans Cronus and Rhea. Hera rules over Mount Olympus as queen of the gods. A matronly figure, Hera served as both the patroness and protectress of ${userInputs[6]} women, presiding over ${userInputs[7]} and blessing marital unions.`
 
 console.log(originalStory);
 document.write(originalStory);
